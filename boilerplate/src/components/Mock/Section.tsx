2a7bbccdd6fa6545bfc95114bfec3eed2365c98b
imysh578 <imysh578@gmail.com>
2022-07-01 20:48:39 +0900

    add bolilerplate

---
+++ boilerplate/src/components/Mock/Section.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styled'
+
+const Section = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Section
---
+++ boilerplate/src/components/Mock/styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { SectionType } from './interface';
+
+export const Container = styled.div<SectionType.Container>`
+  
+`;
